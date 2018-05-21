859400269697d17958d9921bf6c0ef36a22d8e6d
julia sheremet <juliajessicasher@gmail.com>
2018-05-21 10:18:51 -0700

    remove bootstrap dependencies

--- src/main.js
+++ src/main.js
@@ -1,7 +1,5 @@
 // import { } from './scripts.js';
 import $ from 'jquery';
-import 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 
 $(document).ready(function(){
